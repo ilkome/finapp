648c68453beef264e2760ef9f217f3049fb38274
ilkome <ilya.komichev@gmail.com>
2017-01-29 09:31:00 +0300

    Fix conctants

--- app/js/constants.js
+++ app/js/constants.js
@@ -2,8 +2,8 @@ import apiUrl from './apiUrl'
 
 const APP = {
   TRANSACTIONS_URL: `${apiUrl}/transactions`,
-  ACCOUNTS_URL: `${apiUrl}//accounts`,
-  CATEGORIES_URL: `${apiUrl}//categories`
+  ACCOUNTS_URL: `${apiUrl}/accounts`,
+  CATEGORIES_URL: `${apiUrl}/categories`
 }
 
 export default APP
