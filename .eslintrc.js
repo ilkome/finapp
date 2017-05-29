17ad7d5c3da41a7ce8b1d9668951702181a3ef9c
ilkome <ilya.komichev@gmail.com>
2017-05-29 17:09:56 +0300

    Add eslint async await support

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,14 @@
 module.exports = {
-    'extends': 'vue',
-    'rules': {
-      'space-before-function-paren': ['error', {
-        anonymous: 'always',
-        named: 'never',
-        asyncArrow: 'always'
-      }]
-    }
+  'extends': 'vue',
+  'rules': {
+    'space-before-function-paren': ['error', {
+      anonymous: 'always',
+      named: 'never',
+      asyncArrow: 'always'
+    }]
+  },
+  "parserOptions": {
+    "ecmaVersion": 2017,
+    "sourceType": "module"
+  }
 };
