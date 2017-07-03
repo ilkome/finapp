0c3592b8300384e4e759e627f13da190d533cc11
ilkome <ilya.komichev@gmail.com>
2017-07-03 15:42:40 +0300

    Remove unused code

--- webpack.config.js
+++ webpack.config.js
@@ -33,25 +33,6 @@ if (isProd) {
   plugins.push(
     new webpack.HotModuleReplacementPlugin(),
     new FriendlyErrorsPlugin()
-
-    // extract vendor chunks for better caching
-    // new webpack.optimize.CommonsChunkPlugin({
-    //   name: 'vendor',
-    //   minChunks: function (module) {
-    //     // a module is extracted into the vendor chunk if...
-    //     return (
-    //       // it's inside node_modules
-    //       /node_modules/.test(module.context) &&
-    //       // and not a CSS file (due to extract-text-webpack-plugin limitation)
-    //       !/\.css$/.test(module.request)
-    //     )
-    //   }
-    // }),
-    // // extract webpack runtime & manifest to avoid vendor chunk hash changing
-    // // on every build.
-    // new webpack.optimize.CommonsChunkPlugin({
-    //   name: 'manifest'
-    // })
   )
 }
 
