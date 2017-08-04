ffdb1928907ec8f645c2450b12ca51ee3c819e82
ilkome <ilya.komichev@gmail.com>
2017-08-04 12:53:47 +0300

    Fix page loader action

--- src/components/App.vue
+++ src/components/App.vue
@@ -2,10 +2,13 @@
 .app
   //- Loading
   //------------------------------------------------
-  template(v-if="$store.state.pageLoading")
+  template(v-if="$store.state.loader && !$store.state.error")
     transition(name="fade")
       template(v-if="$store.state.error")
-        .loading {{ $store.state.error }}
+        .loading
+          .loading__in
+            .loading__name._error {{ $store.state.error }}
+            .loading__update(@click.prevent="updateAppData()") Update Application
       template(v-else)
         h1.loading Loading...
 
@@ -13,18 +16,8 @@
   //- Loaded
   //------------------------------------------------
   template(v-else)
-    template(v-if="$store.state.loader && !$store.state.error")
-      .loading Loading...
-    template(v-if="$store.state.error")
-      .loading
-        .loading__in
-          .loading__name._error {{ $store.state.error }}
-          .loading__update(@click.prevent="updateAppData()") Update Application
-
     //- Sidebar
-    //------------------------------------------------
     Sidebar
-
     .sidebarToogle(
       v-shortkey="['alt', 'arrowleft']",
       @shortkey="$store.commit('toogleLeftbar')",
@@ -33,24 +26,21 @@
     )
 
     //- main
-    //------------------------------------------------
     .main(:class="$store.state.leftBar.isShow && '_withLeftBar'")
       transition(name="slide")
         router-view
 
     //- TrnForm
-    //------------------------------------------------
     transition(name="slideToLeft")
       .trnForm(v-show="$store.state.trnForm.isShow")
         TrnForm
-
-    a(
+    .trnFormToogle(
       v-shortkey="['alt', 'arrowright']",
       @shortkey="$store.commit('toogleTrnForm')",
       @click.prevent.stop="$store.commit('toogleTrnForm')",
       @mouseover="(e) => toogleTrnForm(e)",
       :class="{_active: $store.state.trnForm.isShow}"
-    ).trnFormToogle
+    )
       .trnFormToogle__icon: .trnFormToogle__icon__in +
 </template>
 
@@ -76,26 +66,11 @@ export default {
         await this.$store.dispatch('getAccounts')
         await this.$store.dispatch('getCategories')
         await this.$store.dispatch('getTrns')
-        this.$store.commit('pageLoaded')
-        this.$store.commit('disableLoader')
+        this.$store.commit('closeLoader')
       } catch (error) {
         this.$store.commit('showError', error.message)
       }
-    },
-
-    toogleSidebar: debounce(function (mouseEvent) {
-      // const isHovered = this.$el.querySelector('.sidebarToogle:hover')
-      // if (isHovered) {
-      //   this.$store.commit('toogleLeftbar')
-      // }
-    }, 200),
-
-    toogleTrnForm: debounce(function (mouseEvent) {
-      // const isHovered = this.$el.querySelector('.trnFormToogle:hover')
-      // if (isHovered) {
-      //   this.$store.commit('toogleTrnForm')
-      // }
-    }, 200)
+    }
   }
 }
 </script>
