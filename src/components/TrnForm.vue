dd49474f5f258bda2e7b624c540a5b592a8cc5a7
Ilya Komichev <ilya.komichev@gmail.com>
2017-09-21 11:42:32 +0300

    Combine trnForm files

--- src/components/TrnForm.vue
+++ src/components/TrnForm.vue
@@ -125,4 +125,250 @@
 
         CategoryCreate
 </template>
-<script src="./trnForm.js"></script>
+
+<script>
+import mathjs from 'mathjs'
+import moment from 'moment'
+import uniqBy from 'lodash/uniqBy'
+import { mapGetters } from 'vuex'
+import { focus } from 'vue-focus'
+import CategoryCreate from './categories/CategoryCreate.vue'
+import CategoryList from './categories/CategoryList.vue'
+
+export default {
+  directives: { focus: focus },
+  components: { CategoryCreate, CategoryList },
+
+  watch: {
+    account() {
+      if (this.account && this.$store.state.trnForm.action === 'create') {
+        this.values.accountId = this.account.id
+        this.values.accountName = this.account.name
+      }
+    },
+    action() {
+      this.fillValues()
+    },
+    updateTrnId() {
+      if (this.updateTrnId) {
+        this.fillValues()
+      }
+    }
+  },
+
+  mounted() {
+    this.fillValues()
+
+    document.addEventListener('keyup', (event) => {
+      if (event.keyCode === 27) { // escape key
+        this.$store.commit('closeTrnForm')
+        this.show.categories = false
+      }
+    })
+  },
+
+  data() {
+    return {
+      isShowEditActions: false,
+      errors: null,
+      show: {
+        categories: false
+      },
+      lastUsedCategories: [],
+      values: {
+        id: null,
+        amount: null,
+        date: moment(),
+        currency: '',
+        type: 0,
+        description: ''
+      }
+    }
+  },
+
+  computed: {
+    ...mapGetters(['trns', 'accounts', 'categories', 'getFilter']),
+
+    action() {
+      return this.$store.state.trnForm.action
+    },
+
+    categoryId() {
+      return this.$store.state.trnForm.categoryId
+    },
+
+    account() {
+      return this.$store.getters.getFilter.account
+    },
+
+    selectedCategory() {
+      const category = this.categories.find(category => category.id === this.categoryId)
+      if (category) {
+        return category
+      }
+      return {}
+    },
+
+    isShowCategories() {
+      return this.$store.state.trnForm.isShowCategories
+    },
+
+    // For fill values
+    updateTrnId() {
+      return this.$store.state.trnForm.updateTrnId
+    }
+  },
+
+  methods: {
+    fillValues() {
+      // Create
+      if (this.$store.state.trnForm.action === 'create') {
+        const lastTrn = this.$store.getters.trns[0]
+        if (lastTrn) {
+          this.$store.commit('setTrnFormCategoryId', lastTrn.categoryId)
+          this.values = {
+            date: moment(),
+            accountId: lastTrn.accountId,
+            accountName: lastTrn.accountName,
+            amount: null,
+            categoryId: lastTrn.categoryId,
+            categoryName: lastTrn.categoryName,
+            categoryIcon: lastTrn.categoryIcon,
+            type: 0,
+            currency: lastTrn.currency,
+            description: ''
+          }
+        }
+      }
+
+      // Update
+      if (this.$store.state.trnForm.action === 'update') {
+        const trn = this.trns.find(trn => trn.id === this.$store.state.trnForm.updateTrnId)
+        this.$store.commit('setTrnFormCategoryId', trn.categoryId)
+        this.values = {
+          id: trn.id,
+          date: moment(trn.date),
+          accountId: trn.accountId,
+          accountName: trn.accountName,
+          amount: trn.amount,
+          categoryId: trn.categoryId,
+          categoryIcon: trn.categoryIcon,
+          type: trn.type,
+          currency: trn.currency,
+          description: trn.description
+        }
+      }
+
+      const lastTrnsUnicCategory = uniqBy(this.trns.slice(0, 50), 'categoryId').slice(0, 15)
+      const lastUsedCategoriesIdsFromTrns = lastTrnsUnicCategory.map(trn => trn.categoryId)
+      this.lastUsedCategories = this.categories
+        .filter(category => lastUsedCategoriesIdsFromTrns.indexOf(category.id) >= 0)
+        .slice(0, 15)
+    },
+
+    toogleCategoriesPop() {
+      this.$store.commit('toogleCategoriesPop')
+    },
+
+    /**
+     * @param {number} categoryId - Id of selected category.
+     */
+    setCategory(categoryId) {
+      const category = this.categories.find(category => category.id === categoryId)
+      if (category) {
+        this.values.categoryId = category.id
+        this.values.categoryName = category.name
+        this.values.categoryIcon = category.icon
+
+        this.$store.commit('setTrnFormCategoryId', categoryId)
+        // Add selected category if it doesn't exist in lastUsedCategories
+        if (!this.lastUsedCategories.find(cat => cat.id === this.categoryId)) {
+          this.lastUsedCategories = [...this.lastUsedCategories.slice(0, 14), this.selectedCategory]
+        }
+      }
+    },
+
+    setTrnType() {
+      this.values.type = (this.values.type === 1) ? 0 : 1
+    },
+
+    setAccound(accountId) {
+      const account = this.accounts.find(account => account.id === accountId)
+      if (account) {
+        this.values.accountId = account.id
+        this.values.accountName = account.name
+        this.values.currency = account.currency
+      }
+    },
+
+    setNextPrevDate(way) {
+      if (way === 'prev') this.values.date = moment(this.values.date).subtract(1, 'days')
+      if (way === 'next') this.values.date = moment(this.values.date).add(1, 'days')
+    },
+
+    async onSubmitForm() {
+      this.$store.commit('showLoader')
+
+      function calc(number) {
+        return mathjs.chain(number.replace(/\s/g, '')).eval().round().value
+      }
+
+      try {
+        const currentTime = moment().format('HH:mm:ss')
+        const day = moment(this.values.date).format('D.MM.YY')
+        const date = moment(`${day} ${currentTime}`, 'D.MM.YY HH:mm:ss').valueOf()
+        let formatedValues = {}
+
+        // Empty
+        if (!this.values.amount) {
+          this.errors = 'Empty amount'
+          this.$store.commit('closeLoader')
+          return
+        }
+        if (!this.values.categoryId) {
+          this.errors = 'Please select category'
+          this.$store.commit('closeLoader')
+          return
+        }
+        if (!this.values.accountId) {
+          this.errors = 'Please select account'
+          this.$store.commit('closeLoader')
+          return
+        }
+
+        const calcAmount = calc(String(this.values.amount))
+        if (calcAmount && calcAmount > 0) {
+          formatedValues = {
+            ...this.values,
+            amount: calcAmount,
+            date
+          }
+          this.errors = false
+        } else {
+          this.errors = 'One amount: wrong number or less than 0'
+        }
+
+        if (!this.errors) {
+          // Create
+          if (this.action === 'create') {
+            await this.$store.dispatch('addTrn', formatedValues)
+            this.values.amount = ''
+            this.values.description = ''
+            this.filter = ''
+          }
+
+          // Update
+          if (this.action === 'update') {
+            await this.$store.dispatch('updateTrn', formatedValues)
+            this.$store.commit('closeTrnForm')
+          }
+        }
+        this.$store.commit('closeLoader')
+      } catch (error) {
+        this.errors = error.message
+        this.$store.commit('closeLoader')
+      }
+    }
+  }
+}
+</script>
