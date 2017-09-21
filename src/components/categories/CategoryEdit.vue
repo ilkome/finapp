42b2544cdce6b392879724b10c4aef19f17d07c7
Ilya Komichev <ilya.komichev@gmail.com>
2017-09-21 11:32:21 +0300

    Add check for same name then edit category

--- src/components/categories/CategoryEdit.vue
+++ src/components/categories/CategoryEdit.vue
@@ -36,20 +36,18 @@ export default {
       const formatedValues = {
         id: values.id,
         name: values.name.trim(),
-        color: values.color ? values.color.trim() : '#000000',
+        color: values.color ? values.color.trim() : '#242424',
         icon: values.icon.trim(),
         parentId: values.parentId
       }
 
       const sameCategory = this.$store.state.categories.all
         .filter(category =>
-          category.id === formatedValues.id &&
           category.name === formatedValues.name &&
-          category.icon === formatedValues.icon &&
           category.parentId === formatedValues.parentId)
 
       if (sameCategory.length) {
-        this.error = 'Same category is already exist!'
+        this.error = 'Same category name is already exist!'
         this.$store.commit('closeLoader')
         return
       }
