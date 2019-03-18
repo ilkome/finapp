e117e185e7d1dd4209ebb86201b500d618db1dec
ilkome <ilya.komichev@gmail.com>
2019-03-18 13:31:19 +0300

    Update ui view when chart diff changes

--- src/components/stat/store/actions.js
+++ src/components/stat/store/actions.js
@@ -1,5 +1,10 @@
 export default {
   async initStat ({ rootState, dispatch, commit }) {
     //
+  },
+
+  setDiffPeriods ({ commit, dispatch }, value) {
+    commit('setDiffPeriods', value)
+    dispatch('saveUiView')
   }
 }
