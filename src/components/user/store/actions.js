351fed9782cf7fd5bf494a9e47cc9ffdb0ef86ef
Ilya Komichev <ilya.komichev@gmail.com>
2020-03-03 15:02:54 +0300

    add opens counts

--- src/components/user/store/actions.js
+++ src/components/user/store/actions.js
@@ -54,6 +54,7 @@ export default {
     db.ref(`users-info/${user.uid}/email`).set(user.email)
     db.ref(`users-info/${user.uid}/uid`).set(user.uid)
     db.ref(`users-info/${user.uid}/loginDate`).set(todayValueOf)
+    db.ref(`users-info/${user.uid}/opens`).set(usersInfoVal.opens ? usersInfoVal.opens + 1 : 1)
 
     // set creation date once
     if (!usersInfoVal || (usersInfoVal && !usersInfoVal.creationDate)) {
