fd0180cc1bd5b4c5885237df49f6da5eeda8a251
Ilya Komichev <ilya.komichev@gmail.com>
2020-03-16 08:16:03 +0300

    add ~ alias to root

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,8 @@ module.exports = {
   configureWebpack: {
     resolve: {
       alias: {
-        '@': path.join(__dirname, 'src')
+        '@': path.join(__dirname, 'src'),
+        '~': path.join(__dirname)
       }
     }
   },
