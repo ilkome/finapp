3dd91d2c95a1ac013c07b01f4fe0a77acdf65753
ilkome <ilya.komichev@gmail.com>
2020-12-11 08:08:21 +0300

    update hotjar

--- components/Metrica.vue
+++ components/Metrica.vue
@@ -1,6 +1,17 @@
 <script>
 export default {
-  name: 'Metrica'
+  name: 'Metrica',
+
+  mounted () {
+    (function(h,o,t,j,a,r){
+      h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+      h._hjSettings={hjid:1947029,hjsv:6};
+      a=o.getElementsByTagName('head')[0];
+      r=o.createElement('script');r.async=1;
+      r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+      a.appendChild(r);
+      })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+  }
 }
 </script>
 
@@ -24,15 +35,5 @@ export default {
 
       <!-- Hotjar Tracking Code for https://finapp.ilko.me -->
     </client-only>
-    <script>
-      (function(h,o,t,j,a,r){
-      h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-      h._hjSettings={hjid:1947029,hjsv:6};
-      a=o.getElementsByTagName('head')[0];
-      r=o.createElement('script');r.async=1;
-      r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-      a.appendChild(r);
-      })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-    </script>
   </div>
 </template>
