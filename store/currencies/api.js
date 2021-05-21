60554ecff5df553affdfd7461e6301f320f01fb0
ilkome <ilya.komichev@gmail.com>
2021-05-21 21:10:23 +0300

    change currencies api server

--- store/currencies/api.js
+++ store/currencies/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const serviceUrl = 'https://api.openrates.io'
+const key = '5976f9fadc5c46e285d764eae24d384b'
+const serviceUrl = `https://openexchangerates.org/api/latest.json?app_id=${key}`
 
 export const getRatesOf = async (baseCurrency) => {
   if (!baseCurrency) {
@@ -8,9 +9,17 @@ export const getRatesOf = async (baseCurrency) => {
     return
   }
 
-  const currencies = await axios.get(`${serviceUrl}/latest?base=${baseCurrency}`)
+  const currencies = await axios.get(serviceUrl)
+  const rates = {}
   if (currencies && currencies.data) {
-    return currencies.data.rates
+    const ratesBasedOnUsd = currencies.data.rates
+    const baseRate = ratesBasedOnUsd[baseCurrency]
+
+    // Conver rates to base currency
+    for (const id in ratesBasedOnUsd) {
+      rates[id] = ratesBasedOnUsd[id] / baseRate
+    }
+    return rates
   }
   else {
     console.log('api unavaliable')
