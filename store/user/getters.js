62d45e1107cc946719c4379808ebbd1fcfe111d2
ilkome <ilya.komichev@gmail.com>
2021-08-08 09:23:23 +0300

    chore (tester): refactor

--- store/user/getters.js
+++ store/user/getters.js
@@ -1,14 +1,12 @@
 export default {
-  userUid (state, getters, rootState) {
-    if (rootState.user && rootState.user.user && rootState.user.user) {
+  userUid (_state, _getters, rootState) {
+    if (rootState.user && rootState.user.user && rootState.user.user)
       return rootState.user.user.uid
-    }
   },
 
   isTester (state) {
-    // eslint-disable-next-line no-undef
     const testerEmail = $nuxt.$config.testerEmail
-    if (state.user && state.user.email === testerEmail) { return true }
-    else { return false }
+    if (state.user && state.user.email === testerEmail) return true
+    else return false
   }
 }
