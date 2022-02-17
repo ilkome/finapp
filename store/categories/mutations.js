5764966ffbdc9ce9c950065624b2c53a86041816
ilkome <ilya.komichev@gmail.com>
2022-02-17 14:40:14 +0300

    chore (categories): refactor

--- store/categories/mutations.js
+++ store/categories/mutations.js
@@ -2,15 +2,14 @@ export default {
   setCategories (state, items) {
     const freezedItems = {}
     if (items) {
-      for (const itemId of Object.keys(items)) {
+      for (const itemId of Object.keys(items))
         freezedItems[itemId] = Object.freeze(items[itemId])
-      }
     }
     state.items = {
       ...freezedItems,
       transfer: {
         color: 'var(--c-blue-1)',
-        icon: 'mdi mdi-mdi mdi-repeat',
+        icon: 'mdi mdi-repeat',
         name: 'Transfer',
         order: 0,
         parentId: 0,
@@ -31,20 +30,14 @@ export default {
   },
 
   setCategoryModalId (state, id) {
-    if (state.modal.id === id) {
-      state.modal.id = null
-    }
-    else {
-      state.modal.id = id
-    }
+    state.modal.id === id
+      ? state.modal.id = null
+      : state.modal.id = id
   },
 
   setCategoryEditId (state, id) {
-    if (state.editId === id) {
-      state.editId = null
-    }
-    else {
-      state.editId = id
-    }
+    state.editId === id
+      ? state.editId = null
+      : state.editId = id
   }
 }
