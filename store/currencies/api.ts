9bcd3e2b6c8ada183d937125e245acaf667fc922
ilkome <ilya.komichev@gmail.com>
2022-03-20 13:58:41 +0700

    fix(currency): api token

--- store/currencies/api.ts
+++ store/currencies/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const key = '5976f9fadc5c46e285d7654eae24d384b'
+const key = '5976f9fadc5c46e285d764eae24d384b'
 const serviceUrl = `https://openexchangerates.org/api/latest.json?app_id=${key}`
 
 type Props = Record<string, string>
