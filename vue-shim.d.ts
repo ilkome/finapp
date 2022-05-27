701eb386222b10132d9cb78871cbe38af5b9440e
ilkome <ilya.komichev@gmail.com>
2022-05-27 19:54:30 +0700

    types: add i18n

--- vue-shim.d.ts
+++ vue-shim.d.ts
@@ -1,4 +1,12 @@
+import type { NuxtI18nInstance } from '@nuxtjs/i18n'
+
 declare module '*.vue' {
   import Vue from 'vue'
   export default Vue
+}
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    i18n: NuxtI18nInstance
+  }
 }
