56b64e9a51914b0f5aa0dc14f25528bf27cc4eac
ilkome <ilya.komichev@gmail.com>
2022-05-30 14:15:30 +0700

    build(gulp ftp): add json to upload min task

--- gulpfile.js
+++ gulpfile.js
@@ -48,7 +48,7 @@ function uploadAll(cb) {
 }
 
 function uploadMin(cb) {
-  return uploadTask('dist/**/*.{css,js,html}', cb)
+  return uploadTask('dist/**/*.{css,js,json,html}', cb)
 }
 
 // upload task
