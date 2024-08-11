7558dbc3473d23e25c6e52cca9dd726a9361aa87
Ilya Komichev <ilya.komichev@gmail.com>
2024-08-11 13:48:18 +0300

    chore: Refactor MiniItem.vue to improve code readability and maintainability

--- app/components/stat/MiniItem.vue
+++ app/components/stat/MiniItem.vue
@@ -2,10 +2,11 @@
 import dayjs from 'dayjs'
 import defu from 'defu'
 import { useStorage } from '@vueuse/core'
-import type { CategoryId } from '../categories/types'
+import type { CategoryId, CategoryItem } from '../categories/types'
+import { seriesOptions } from '~/components/stat/chart/config2'
 import type { ChartType } from '~/components/stat/chart/types'
 import type { FilterProvider } from '~/components/filter/useFilter'
-import type { FullDuration, Interval, Range } from '~/components/date/types'
+import type { Range } from '~/components/date/types'
 import { type MoneyTypeSlugSum, type ViewOptions, ViewOptionsSchema, defaultViewOptions } from '~/components/stat/types'
 import type { TrnId } from '~/components/trns/types'
 import useAmount from '~/components/amount/useAmount'
@@ -13,8 +14,8 @@ import { getStyles } from '~/components/ui/getStyles'
 import { markArea } from '~/components/stat/chart/utils'
 import { useCategoriesStore } from '~/components/categories/useCategories'
 import { useDateRange } from '~/components/date/useDateRange'
-import { useNewStat } from '~/components/stat/useNewStat'
 import { useTrnsStore } from '~/components/trns/useTrnsStore'
+import type { TotalReturns } from '~/components/amount/getTotal'
 
 const props = defineProps<{
   isQuickModal?: boolean
@@ -28,7 +29,6 @@ const props = defineProps<{
 const filter = inject('filter') as FilterProvider
 const trnsStore = useTrnsStore()
 const { getTotalOfTrnsIds } = useAmount()
-const { getCats, getSeries } = useNewStat()
 const categoriesStore = useCategoriesStore()
 const intervalRange = useDateRange({
   key: `finapp-${props.type}${props.storageKey}-${JSON.stringify(filter.catsIds.value)}`,
@@ -80,6 +80,111 @@ onBeforeMount(() => {
   }
 })
 
+function sortCategoriesByAmount(a: TotalCategory, b: TotalCategory) {
+  if (!a || !b)
+    return 0
+
+  // Sort positive values from biggest to smallest
+  if (a.value >= 0 && b.value >= 0) {
+    return b.value - a.value
+  }
+  // Sort negative values from smallest to biggest
+  else if (a.value < 0 && b.value < 0) {
+    return a.value - b.value
+  }
+  // Keep positive values before negative values
+  else if (a.value >= 0 && b.value < 0) {
+    return -1
+  }
+  // Keep negative values after positive values
+  else {
+    return 1
+  }
+}
+
+export type TotalCategories = {
+  expense: TotalCategory[]
+  income: TotalCategory[]
+}
+
+export type TotalCategory = {
+  id: CategoryId
+  trnsIds: TrnId[]
+  value: number
+}
+
+function getCats(trnsIds: TrnId[], isGroupedByParent?: boolean, preCategoriesIds?: CategoryId[]) {
+  const categoriesWithTrns = trnsIds?.reduce(
+    (acc, trnId) => {
+      let newCategoryId = trnsStore.items[trnId]?.categoryId
+      if (!newCategoryId)
+        return acc
+
+      if (categoriesStore.transferCategoriesIds.includes(newCategoryId))
+        return acc
+
+      if (isGroupedByParent) {
+        const trnBaseCategory: CategoryItem = categoriesStore.items[newCategoryId]
+
+        newCategoryId = trnBaseCategory?.parentId === 0
+          ? trnsStore.items[trnId]?.categoryId
+          : trnBaseCategory?.parentId
+      }
+
+      acc[newCategoryId] ??= []
+      acc[newCategoryId].push(trnId)
+      return acc
+    },
+    {} as Record<CategoryId, TrnId[]>,
+  )
+
+  const categories = Object.keys(categoriesWithTrns)
+    .reduce(
+      (acc, categoryId) => {
+        const totalInCategory = getTotalOfTrnsIds(categoriesWithTrns[categoryId]!)
+        const trnsIdsInCategory = categoriesWithTrns[categoryId]!
+
+        acc.push({
+          id: categoryId,
+          trnsIds: trnsIdsInCategory,
+          value: totalInCategory.sum,
+        })
+        return acc
+      },
+      [] as TotalCategory[],
+    )
+    .sort((a, b) => sortCategoriesByAmount(a, b))
+
+  if (preCategoriesIds && preCategoriesIds.length > 0) {
+    preCategoriesIds.forEach((id) => {
+      if (!categoriesWithTrns[id]) {
+        categories.push({
+          id,
+          trnsIds: [],
+          value: 0,
+        })
+      }
+    })
+  }
+  return categories
+}
+
+function getSeries(
+  total: TotalReturns[],
+  type: MoneyTypeSlugSum,
+  ranges: Range[],
+) {
+  // const types = type === 'sum' ? ['income', 'expense', 'sum'] : [type]
+  const types = type === 'sum' ? ['expense', 'income'] : [type]
+
+  return types.map((t, idx) => ({
+    color: seriesOptions[t].color,
+    cursor: 'default',
+    data: total.map(i => t !== 'sum' ? Math.abs(i[t]) : i[t]),
+    name: ranges[idx]?.start,
+    type: seriesOptions[t].type,
+  }))
+}
 /**
  * Chart
  */
