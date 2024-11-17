fe0cb8b7349142fc45d625004c61d23efa542fa1
Ilya Komichev <ilya.komichev@gmail.com>
2024-11-17 15:33:48 +0300

    2024.11.17-3:33 PM

--- app/plugins/dayjs.ts
+++ app/plugins/dayjs.ts
@@ -15,8 +15,6 @@ dayjs.extend(isYesterday)
 export default defineNuxtPlugin((nuxtApp) => {
   const locale = nuxtApp.$i18n.locale.value === 'en' ? en : ru
 
-  console.log(locale)
-
   dayjs.locale({
     ...locale,
     weekStart: 1,
