351b84dfa86f22a9e2b8a12e80c2caa2bec3a7be
Ilya Komichev <ilya.komichev@gmail.com>
2024-11-21 17:16:25 +0300

    2024.11.21-17:16

--- app/components/stat/date/useGetDateRange.ts
+++ app/components/stat/date/useGetDateRange.ts
@@ -1,125 +1,85 @@
 import dayjs from 'dayjs'
 import type { Range, StatDateParams } from '~/components/date/types'
 
+type DateFormatParams = {
+  by: StatDateParams['rangeBy']
+  duration: StatDateParams['rangeDuration']
+  end: dayjs.Dayjs
+  start: dayjs.Dayjs
+  type: 'start' | 'end'
+}
+
 export function useGetDateRange(t: (key: string) => string) {
   const today = dayjs()
 
-  function calculateDate({
-    by,
-    duration,
-    end,
-    start,
-    type,
-  }: {
-    by: StatDateParams['rangeBy']
-    duration: StatDateParams['rangeDuration']
-    end: dayjs.Dayjs
-    start: dayjs.Dayjs
-    type: 'start' | 'end'
-  }) {
-    // Year
-    if (by === 'year') {
-      if (duration === 1) {
-        if (start.isSame(today, 'year')) {
-          return type === 'start' ? t('dates.year.current') : ''
-        }
+  function handleCurrentPeriod(by: StatDateParams['rangeBy'], type: 'start' | 'end') {
+    return type === 'start' ? t(`dates.${by}.current`) : ''
+  }
 
-        if (start.isSame(today.subtract(1, 'year'), 'year'))
-          return type === 'start' ? t('dates.year.last') : ''
-      }
+  function handleLastPeriod(by: StatDateParams['rangeBy'], type: 'start' | 'end') {
+    return type === 'start' ? t(`dates.${by}.last`) : ''
+  }
 
-      if (end.isSame(today, 'year'))
-        return type === 'start' ? '' : `${t('dates.last')} ${duration} ${t('dates.year.simple')}`
+  function handleLastNPeriods(by: StatDateParams['rangeBy'], duration: number, type: 'start' | 'end') {
+    return type === 'start' ? '' : `${t('dates.last')} ${duration} ${t(`dates.${by}.simple`)}`
+  }
 
-      if (type === 'start')
-        return duration === 1 ? start.format('YYYY') : `${start.format('YYYY')} - ${end.format('YYYY')}`
-      else
-        return ''
-    }
+  function calculateDate({ by, duration, end, start, type }: DateFormatParams): string {
+    // Single duration cases
+    if (duration === 1) {
+      if (start.isSame(today, by))
+        return handleCurrentPeriod(by, type)
 
-    // Month
-    if (by === 'month') {
-      if (duration === 1) {
+      if (start.isSame(today.subtract(1, by), by))
+        return handleLastPeriod(by, type)
+
+      // Special case for single day in current year/month
+      if (by === 'day' && start.isSame(today, 'year')) {
         if (start.isSame(today, 'month'))
-          return type === 'start' ? t('dates.month.current') : ''
-
-        if (start.isSame(today.subtract(1, 'month'), 'month'))
-          return type === 'start' ? t('dates.month.last') : ''
-      }
-
-      if (end.isSame(today, 'month'))
-        return type === 'start' ? '' : `${t('dates.last')} ${duration} ${t('dates.month.simple')}`
-
-      if (start.isSame(end, 'year')) {
-        if (start.isSame(end, 'month'))
-          return type === 'start' ? start.format('MMMM YYYY') : ''
-
-        return type === 'start' ? start.format('MMMM') : ` - ${end.format('MMMM YYYY')}`
-      }
-
-      return type === 'start' ? start.format('MMMM YYYY') : ` - ${end.format('MMMM YYYY')}`
-    }
-
-    // Week
-    if (by === 'week') {
-      if (duration === 1) {
-        if (start.isSame(today, 'week'))
-          return type === 'start' ? t('dates.week.current') : ''
-
-        if (start.isSame(today.subtract(1, 'week'), 'week'))
-          return type === 'start' ? t('dates.week.last') : ''
-      }
-
-      if (end.isSame(today, 'week'))
-        return type === 'start' ? '' : `${t('dates.last')} ${duration} ${t('dates.week.simple')}`
-
-      if (start.isSame(end, 'year')) {
-        if (start.isSame(end, 'month'))
-          return type === 'start' ? start.format('D') : ` - ${end.format('D MMMM YYYY')}`
-
-        return type === 'start' ? start.format('D MMMM') : ` - ${end.format('D MMMM YYYY')}`
-      }
-
-      return type === 'start' ? start.format('D MMMM YYYY') : ` - ${end.format('D MMMM YYYY')}`
-    }
-
-    // Day
-    if (by === 'day') {
-      if (duration === 1) {
-        if (start.isSame(today, 'day'))
-          return type === 'start' ? t('dates.day.current') : ''
-
-        if (start.isSame(today.subtract(1, 'day'), 'day'))
-          return type === 'start' ? t('dates.day.last') : ''
-
-        if (start.isSame(today, 'year')) {
-          if (start.isSame(today, 'month'))
-            return type === 'start' ? start.format('D MMMM') : ''
-
           return type === 'start' ? start.format('D MMMM') : ''
+        return type === 'start' ? start.format('D MMMM') : ''
+      }
+    }
+
+    // Last N periods case
+    if (end.isSame(today, by))
+      return handleLastNPeriods(by, duration, type)
+
+    // Specific format cases
+    switch (by) {
+      case 'year':
+        if (type === 'start')
+          return duration === 1 ? start.format('YYYY') : `${start.format('YYYY')} - ${end.format('YYYY')}`
+        return ''
+
+      case 'month':
+        if (start.isSame(end, 'year')) {
+          if (start.isSame(end, 'month'))
+            return type === 'start' ? start.format('MMMM YYYY') : ''
+          return type === 'start' ? start.format('MMMM') : ` - ${end.format('MMMM YYYY')}`
         }
+        return type === 'start' ? start.format('MMMM YYYY') : ` - ${end.format('MMMM YYYY')}`
 
-        return type === 'start' ? start.format('D MMMM YYYY') : ''
-      }
+      case 'week':
+        if (start.isSame(end, 'year')) {
+          if (start.isSame(end, 'month'))
+            return type === 'start' ? start.format('D') : ` - ${end.format('D MMMM YYYY')}`
+          return type === 'start' ? start.format('D MMMM') : ` - ${end.format('D MMMM YYYY')}`
+        }
+        return type === 'start' ? start.format('D MMMM YYYY') : ` - ${end.format('D MMMM YYYY')}`
 
-      if (end.isSame(today, 'day'))
-        return type === 'start' ? '' : `${t('dates.last')} ${duration} ${t('dates.day.simple')}`
-
-      if (start.isSame(today, 'year')) {
-        if (start.isSame(end, 'month'))
-          return type === 'start' ? start.format('D') : `-${end.format('D MMMM')}`
-
-        return type === 'start' ? start.format('D MMMM') : ` - ${end.format('D MMMM')}`
-      }
-
-      if (start.isSame(end, 'year')) {
-        if (start.isSame(end, 'month'))
-          return type === 'start' ? start.format('D') : `-${end.format('D MMMM YYYY')}`
-
-        return type === 'start' ? start.format('D MMMM') : ` - ${end.format('D MMMM YYYY')}`
-      }
-
-      return type === 'start' ? start.format('D MMMM YYYY') : ` - ${end.format('D MMMM YYYY')}`
+      case 'day':
+        if (start.isSame(today, 'year')) {
+          if (start.isSame(end, 'month'))
+            return type === 'start' ? start.format('D') : `-${end.format('D MMMM')}`
+          return type === 'start' ? start.format('D MMMM') : ` - ${end.format('D MMMM')}`
+        }
+        if (start.isSame(end, 'year')) {
+          if (start.isSame(end, 'month'))
+            return type === 'start' ? start.format('D') : `-${end.format('D MMMM YYYY')}`
+          return type === 'start' ? start.format('D MMMM') : ` - ${end.format('D MMMM YYYY')}`
+        }
+        return type === 'start' ? start.format('D MMMM YYYY') : ` - ${end.format('D MMMM YYYY')}`
     }
   }
 
