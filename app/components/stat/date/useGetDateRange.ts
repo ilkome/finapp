228cd5ebfb74e675d3bcac548eb16f6bd06f88c6
Ilya Komichev <ilya.komichev@gmail.com>
2024-11-21 17:12:41 +0300

    2024.11.21-17:12

--- app/components/stat/date/useGetDateRange.ts
+++ app/components/stat/date/useGetDateRange.ts
@@ -47,67 +47,79 @@ export function useGetDateRange(t: (key: string) => string) {
           return type === 'start' ? t('dates.month.last') : ''
       }
 
-      if (start.isSame(today, 'year'))
-        return type === 'start' ? start.format('MMMM') : ''
+      if (end.isSame(today, 'month'))
+        return type === 'start' ? '' : `${t('dates.last')} ${duration} ${t('dates.month.simple')}`
 
-      if (type === 'start')
-        return start.format('MMMM YYYY')
-      else
-        return ''
+      if (start.isSame(end, 'year')) {
+        if (start.isSame(end, 'month'))
+          return type === 'start' ? start.format('MMMM YYYY') : ''
+
+        return type === 'start' ? start.format('MMMM') : ` - ${end.format('MMMM YYYY')}`
+      }
+
+      return type === 'start' ? start.format('MMMM YYYY') : ` - ${end.format('MMMM YYYY')}`
     }
 
     // Week
     if (by === 'week') {
-      if (end.isSame(today, 'week')) {
-        if (duration === 1)
-          return type === 'end' ? t('dates.week.current') : ''
+      if (duration === 1) {
+        if (start.isSame(today, 'week'))
+          return type === 'start' ? t('dates.week.current') : ''
 
-        return type === 'end' ? `${t('dates.last')} ${duration} ${t('dates.week.simple')}` : ''
+        if (start.isSame(today.subtract(1, 'week'), 'week'))
+          return type === 'start' ? t('dates.week.last') : ''
       }
 
-      if (end.isSame(today.subtract(1, 'week'), 'week'))
-        return type === 'start' ? t('dates.week.last') : ''
+      if (end.isSame(today, 'week'))
+        return type === 'start' ? '' : `${t('dates.last')} ${duration} ${t('dates.week.simple')}`
 
       if (start.isSame(end, 'year')) {
-        return type === 'start' ? start.format('D MMM') : ` - ${end.format('D MMM')}`
+        if (start.isSame(end, 'month'))
+          return type === 'start' ? start.format('D') : ` - ${end.format('D MMMM YYYY')}`
+
+        return type === 'start' ? start.format('D MMMM') : ` - ${end.format('D MMMM YYYY')}`
       }
 
-      return type === 'start' ? start.format('D MMM YYYY') : ` - ${end.format('D MMM YYYY')}`
+      return type === 'start' ? start.format('D MMMM YYYY') : ` - ${end.format('D MMMM YYYY')}`
     }
 
     // Day
     if (by === 'day') {
-      if (start.isSame(today, 'day') && duration === 1)
-        return type === 'start' ? t('dates.day.current') : ''
+      if (duration === 1) {
+        if (start.isSame(today, 'day'))
+          return type === 'start' ? t('dates.day.current') : ''
+
+        if (start.isSame(today.subtract(1, 'day'), 'day'))
+          return type === 'start' ? t('dates.day.last') : ''
+
+        if (start.isSame(today, 'year')) {
+          if (start.isSame(today, 'month'))
+            return type === 'start' ? start.format('D MMMM') : ''
+
+          return type === 'start' ? start.format('D MMMM') : ''
+        }
+
+        return type === 'start' ? start.format('D MMMM YYYY') : ''
+      }
 
       if (end.isSame(today, 'day'))
-        return type === 'start' ? `${t('dates.last')} ${duration} ${t('dates.day.simple')}` : ''
-
-      if (end.isSame(today.subtract(1, 'day'), 'day'))
-        return type === 'start' ? t('dates.day.last') : ''
+        return type === 'start' ? '' : `${t('dates.last')} ${duration} ${t('dates.day.simple')}`
 
       if (start.isSame(today, 'year')) {
-        if (start.isSame(end, 'month')) {
-          if (duration === 1)
-            return type === 'start' ? start.format('D') : ''
+        if (start.isSame(end, 'month'))
+          return type === 'start' ? start.format('D') : `-${end.format('D MMMM')}`
 
-          if (start.isSame(end, 'month'))
-            return type === 'start' ? start.format('D') : `-${end.format('D MMM')}`
-        }
-        return type === 'start' ? start.format('D MMM') : ` - ${end.format('D MMM')}`
+        return type === 'start' ? start.format('D MMMM') : ` - ${end.format('D MMMM')}`
       }
 
       if (start.isSame(end, 'year')) {
         if (start.isSame(end, 'month'))
-          return type === 'start' ? start.format('D') : `-${end.format('D MMM YYYY')}`
+          return type === 'start' ? start.format('D') : `-${end.format('D MMMM YYYY')}`
 
-        return type === 'start' ? start.format('D MMM') : ` - ${end.format('D MMM YYYY')}`
+        return type === 'start' ? start.format('D MMMM') : ` - ${end.format('D MMMM YYYY')}`
       }
 
-      if (start.isSame(end, 'month'))
-        return type === 'start' ? start.format('D') : `-${end.format('D MMM YYYY')}`
-
-      return type === 'start' ? start.format('D MMM YYYY') : ` - ${end.format('D MMM YYYY')}`
+      return type === 'start' ? start.format('D MMMM YYYY') : ` - ${end.format('D MMMM YYYY')}`
     }
   }
 
