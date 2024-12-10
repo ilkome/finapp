068fb5683636eaacb531cac20add8719ca751084
Ilya Komichev <ilya.komichev@gmail.com>
2024-12-10 12:06:48 +0300

    2024.12.10-12:06

---
+++ app/pages/200.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+useHead({ title: '200' })
+</script>
+
+<template>
+  <div class="font-secondary flex h-dvh items-center justify-center text-5xl">
+    200
+  </div>
+</template>
