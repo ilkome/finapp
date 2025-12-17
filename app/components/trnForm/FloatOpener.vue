21d67918eebe113e420699fe41c65361a7c91ff7
Ilya Komichev <ilya.komichev@gmail.com>
2025-12-17 15:26:49 +0300

    chore(TrnFromOpener): improve layout and styling

--- app/components/trnForm/FloatOpener.vue
+++ app/components/trnForm/FloatOpener.vue
@@ -7,19 +7,20 @@ const { onClose, trnFormCreate, ui } = useTrnsFormStore()
 <template>
   <div
     class="
-      group absolute bottom-0 right-0 z-20 hidden h-full items-end justify-center transition
-      hover:bg-[var(--item-5)] 2xl:flex
+      group absolute right-0 bottom-0 hidden h-full w-[300px] items-end justify-end
+      transition hover:bg-[var(--item-5)] xl:flex
     "
     @click="ui.isShow ? onClose() : trnFormCreate()"
   >
     <div
       class="
-        _group-hover:bg-(--ui-primary)
-        flex items-center justify-center
-        rounded-full px-3
-        py-4 text-2xl text-icon-primary
+        text-icon-primary flex items-center
+        justify-center rounded-full
+        px-4 py-5 text-2xl
+        opacity-70
         transition
         group-hover:scale-125
+        group-hover:opacity-100
       "
     >
       <Icon name="hugeicons:plus-sign-square" />
